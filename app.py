cc2cca096f7cc67bfffe126da8823e304369e92a
Vishwet Nadimetla <103368238+mr-vishwet@users.noreply.github.com>
2023-03-19 21:23:01 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+import PyPDF2
+import re
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('error.html')
+
+
+@app.route('/troubleshoot')
+def troubleshoot():
+    return app.send_static_file('sample.pdf')
+
+
+if __name__ == "__main__":
+    # This line will start the Flask app on port 8080
+    app.run(host='0.0.0.0', port=8080)
